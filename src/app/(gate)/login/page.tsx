96da8d50da7d56895661266c9b703f1ec883368b
ROG <luan.roger.2003@gmail.com>
2025-07-14 18:30:37 -0300

    feat: add breakpoint to desktop
    mobile first responsiveness

--- src/app/(gate)/login/page.tsx
+++ src/app/(gate)/login/page.tsx
@@ -3,9 +3,9 @@ import LoginForm from "@/components/login-form";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <div className="card w-1/2 bg-amber-100">
-        <div className="card-body">
+    <div className="flex min-h-screen flex-col items-center gap-4 lg:justify-center">
+      <div className="card size-full grow bg-amber-100 lg:size-fit lg:w-1/2 lg:grow-0">
+        <div className="card-body flex flex-col items-center justify-center">
           <div className="flex w-full flex-col items-center gap-2">
             <AthenaIcon />
             <h1 className="text-3xl font-bold">Login</h1>
--- src/app/(gate)/register/page.tsx
+++ src/app/(gate)/register/page.tsx
@@ -2,9 +2,9 @@ import RegisterForm from "@/components/register-form";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <div className="card w-1/2 bg-amber-100">
-        <div className="card-body flex flex-col">
+    <div className="flex min-h-screen flex-col items-center gap-4 lg:justify-center">
+      <div className="card size-full grow bg-amber-100 lg:size-fit lg:w-1/2 lg:grow-0">
+        <div className="card-body flex flex-col items-center justify-center">
           <h1 className="self-center text-3xl font-bold">Registro</h1>
           <RegisterForm />
         </div>
