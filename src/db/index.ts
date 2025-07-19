5264f43790a1ddb102d9da8aa0ab4559cdbb320d
ROG <luan.roger.2003@gmail.com>
2025-07-19 10:26:35 -0300

    feat: add schemas to db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,4 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { schemas } from "./schemas";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL!, { schema: schemas });
