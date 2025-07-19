b674821480264c02a4e74756d413d52f53b53d81
ROG <luan.roger.2003@gmail.com>
2025-07-19 17:25:02 -0300

    chore: revert the dashboard page

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import { getUser } from "@/app/actions/auth";
-import NewFileForm from "@/components/new-file-form";
 import SignOutButton from "@/components/sign-out-button";
 import { redirect } from "next/navigation";
 
@@ -15,7 +14,6 @@ export default async function Page() {
     <main className="flex flex-col items-start gap-4">
       <h1>{name}</h1>
       <p>{email}</p>
-      <NewFileForm />
       <SignOutButton />
     </main>
   );
