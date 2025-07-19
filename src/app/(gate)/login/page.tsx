61c40ca71452ec35b17622f66ee4a5511ef3c4da
csvitor-dev <csvitor.dev@gmail.com>
2025-07-19 15:25:32 -0300

    fix: correct container responsiveness

--- src/app/(gate)/login/page.tsx
+++ src/app/(gate)/login/page.tsx
@@ -3,8 +3,8 @@ import LoginForm from "@/components/login-form";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-4 lg:justify-center">
-      <div className="card bg-secondary-content card-lg size-full grow lg:size-fit lg:w-1/2 lg:grow-0">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <div className="card bg-secondary-content card-lg w-md md:w-lg lg:w-[30%] lg:grow-0">
         <div className="card-body flex flex-col items-center justify-center">
           <div className="flex w-full flex-col items-center gap-2">
             <AthenaIcon />
--- src/app/(gate)/register/page.tsx
+++ src/app/(gate)/register/page.tsx
@@ -1,14 +1,18 @@
+import { AthenaIcon } from "@/components/icons";
 import RegisterForm from "@/components/register-form";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center gap-4 lg:justify-center">
-      <div className="card bg-secondary-content card-lg size-full grow lg:size-fit lg:w-1/2 lg:grow-0">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <div className="card bg-secondary-content card-lg w-md md:w-lg lg:w-[30%] lg:grow-0">
         <div className="card-body flex flex-col items-center justify-center">
-          <h1 className="card-title">Registro</h1>
+          <div className="flex w-full flex-col items-center gap-2">
+            <AthenaIcon />
+            <h1 className="card-title">Registro</h1>
+          </div>
           <RegisterForm />
         </div>
       </div>
-    </div>
+    </main>
   );
 }
