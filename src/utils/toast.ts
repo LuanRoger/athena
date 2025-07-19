519d46cca1aa0bb809204dfc0a4a9e138f4b0a40
ROG <luan.roger.2003@gmail.com>
2025-07-19 09:00:25 -0300

    feat: add toast

---
+++ src/utils/toast.ts
@@ -0,0 +1,27 @@
+import { AppMessages } from "@/constants";
+import { NoDataActionResult } from "@/models";
+import { toast } from "sonner";
+
+export function showSuccessToast(message: string) {
+  toast.success(message);
+}
+
+export function showInfoToast(message: string) {
+  toast(message);
+}
+
+export function showErrorToast(message: string) {
+  toast.error(message);
+}
+
+export function showToastByActionResult(
+  result: NoDataActionResult,
+  showSuccess = true,
+  showError = true,
+) {
+  if (result.success && showSuccess) {
+    toast.success(result.successMessage ?? AppMessages.GENERIC_TOAST_SUCCESS);
+  } else if (showError) {
+    toast.error(result.error ?? AppMessages.GENERIC_TOAST_ERROR);
+  }
+}
