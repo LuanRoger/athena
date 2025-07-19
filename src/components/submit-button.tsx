c4e6e9acb357ff7d8a0456f19b1d592ea3b189db
ROG <luan.roger.2003@gmail.com>
2025-07-19 15:01:09 -0300

    feat: add cn

--- src/components/submit-button.tsx
+++ src/components/submit-button.tsx
@@ -1,16 +1,25 @@
 "use client";
 
+import { cn } from "@/utils/tailwind";
 import { useFormStatus } from "react-dom";
 
 interface SubmitButtonProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export default function SubmitButton({ children }: SubmitButtonProps) {
+export default function SubmitButton({
+  children,
+  className,
+}: SubmitButtonProps) {
   const { pending } = useFormStatus();
 
   return (
-    <button type="submit" className="btn btn-primary" disabled={pending}>
+    <button
+      type="submit"
+      className={cn("btn btn-primary", className)}
+      disabled={pending}
+    >
       {children}
     </button>
   );
