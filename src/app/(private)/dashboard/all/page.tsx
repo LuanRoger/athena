da9d6f600f80bbbc79d53164c8e332f2d17ff6fb
ROG <luan.roger.2003@gmail.com>
2025-07-26 10:01:48 -0300

    feat: update page with form

--- src/app/(private)/dashboard/all/page.tsx
+++ src/app/(private)/dashboard/all/page.tsx
@@ -1,9 +1,25 @@
+import GeneralFilesFilter from "@/components/general-files-filter";
 import GeneralFilesGrid from "@/components/general-files-grid";
 
-export default function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    tag?: number;
+    titleTerm?: string;
+    authorTerm?: string;
+  }>;
+}) {
+  const { tag, titleTerm, authorTerm } = await searchParams;
+
   return (
-    <main className="p-4">
-      <GeneralFilesGrid />
+    <main className="flex flex-col gap-4 p-4">
+      <GeneralFilesFilter />
+      <GeneralFilesGrid
+        tag={tag}
+        titleTerm={titleTerm}
+        authorTerm={authorTerm}
+      />
     </main>
   );
 }
